8763ff08ad00ce98b2761fc7587acb88921c8c7f
Diksi <lmidorunl@gmail.com>
2021-11-18 22:09:09 +0400

    solve tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
 exports.min = function min (array) {
-  return 0;
+  if ( array === undefined || array.length === 0){
+    return 0;
+  }
+  let min = array[0];
+  for (let v of array){
+    if (min >= v){
+      min = v;
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array === undefined || array.length === 0){
+    return 0;
+  }
+  let max = array[0];
+  for (let v of array){
+    if (max <= v){
+      max = v;
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array === undefined || array.length === 0){
+    return 0;
+  }
+  let res = 0;
+  for (let v of array){
+    res += v;
+  }
+  return res / array.length;
 }
